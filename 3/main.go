3b9543211825c3eb1fc2be74bf064198bc067aad
Rizqon Maulana <rizqonmaulana5@gmail.com>
2021-04-05 12:10:44 +0800

    web service

--- 3/main.go
+++ 3/main.go
@@ -4,21 +4,79 @@ import (
     "fmt"
     "log"
     "net/http"
-	"io/ioutil"
+    "strconv"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	bodyBytes, _ := ioutil.ReadAll(r.Body)
-	bodyString := string(bodyBytes)
-	fmt.Fprintf(w, bodyString)
+// func for referse integer
+func reverseInt(value int) int {
+	// conver int to string
+    intString := strconv.Itoa(value)
+
+    newString := ""
+
+	// loop the string and reverse it by index
+    for x := len(intString); x > 0; x-- {
+        newString += string(intString[x - 1])
+    }
+
+	// conver to integer
+    newInt, err := strconv.Atoi(newString)
+
+    if(err != nil){
+        fmt.Println("Error converting string to int")
+    }
+
+    return newInt
 }
 
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+//func palindromCheck is to check is the input a palindrom or not
+func palindromCheck(i int, reverse int) bool {
+	if i == reverse {
+	// if i & reverse value is same return true to tell that the input is a palindrom
+		return true
+	} else {
+	// if i & reverse value is not same return false to tell that the input is not a palindrom
+		return false
+	}
 }
+
+func loop(int1 int, int2 int) int {
+	// default value of result is 0, it will increase if there is a palindrom
+	result := 0
+	for i := int1; i <= int2; i++ {
+		// call reverseInt function to reverse the value from loop
+		reverse := reverseInt(i)
+		
+		// call palindromCheck to check is the value from the loop and reverse is same 
+		check := palindromCheck(i,reverse)
+
+		// if the value from loop and reverse is same plus 1 to result
+		if check {
+			result += 1
+		}
+	}
+	return result
+}
+
+
+
+func palindrom(w http.ResponseWriter, r *http.Request) {
+    input1 := 1
+    input2 := 10
+    result := loop(input1, input2)
+
+    input1Str := strconv.Itoa(input1)
+    input2Str := strconv.Itoa(input2)
+    resultStr := strconv.Itoa(result)
+
+    fmt.Fprintf(w, "Palindrom, input : '" + input1Str + " " + input2Str + "' result = " + resultStr) // write data to response
+}
+
 
 func main() {
-    handleRequests()
+    http.HandleFunc("/", palindrom) // setting router rule
+    err := http.ListenAndServe(":9090", nil) // setting listening port
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
